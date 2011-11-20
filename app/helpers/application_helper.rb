7872129a2c10d50436985b7d5197dccc08014dbf
Dmitry Drobotov <extern.fx@gmail.com>
2011-11-20 12:53:27 +0300

    Java Url Added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
+  
+  ApplicationHelper::JAVA_URL = "http://192.168.1.8:8084/PersonnelService/graphs.do"
+  
   def user_full_name_link_to(user)
     link_to user.full_name, user_path(user) if user.present?
   end
+
+  def java_url(params = {})
+    ApplicationHelper::JAVA_URL + "?" + params.to_query
+  end
+
 end
